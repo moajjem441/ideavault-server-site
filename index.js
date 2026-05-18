c9e55ccb6c97152c16d9f273e4134db3f2e1536d
Moajjem Hossain <moajjem441@gmail.com>
2026-05-18 21:44:09 +0600

    database connected

--- index.js
+++ index.js
@@ -21,14 +21,11 @@ const client = new MongoClient(uri, {
 });
 
 
-// // db.movies.aggregate([
-// //    { $limit : 5 }
-// // ])
 
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//   await client.connect();
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+  await client.connect();
 
 //    const db = client.db('ideaVault'); 
 //     const trendingCollection = db.collection("trendingIdea");
@@ -44,15 +41,15 @@ const client = new MongoClient(uri, {
 
 
   
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
