0e8dcf8f6ec6b59ae8125dbaa9f59233c32ef521
Moajjem Hossain <moajjem441@gmail.com>
2026-05-23 12:55:14 +0600

    all-comments get api creaed

--- index.js
+++ index.js
@@ -202,7 +202,8 @@ app.get('/ideas', async (req, res) => {
 
 
 //----------------------<>Comments<>--------------
-   app.get('/all-comments',async(req,res)=>{
+   app.get('/all-comments',async(req,res)=>
+    
     //  const {ideaId}=req.params
       const allComments= await commentCollection.find().toArray()
       res.json(allComments)
