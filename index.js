a6ac1e231ab344dd5352b6b73a444181d026d792
Moajjem Hossain <moajjem441@gmail.com>
2026-05-23 22:17:09 +0600

    .env updated

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const verifyToken =async (req,res,next)=>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-  await client.connect();
+  // await client.connect();
 
    const db = client.db('ideaVault'); 
     const trendingCollection = db.collection("trendingIdea");
@@ -290,7 +290,8 @@ app.delete('/all-comments/:id',async(req,res)=>{
 
   
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+    
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
