b39314893b5bcbeb4ed73b11030a0ed647a6a959
Moajjem Hossain <moajjem441@gmail.com>
2026-05-24 20:04:16 +0600

    code updated

--- index.js
+++ index.js
@@ -206,6 +206,7 @@ app.get('/ideas', async (req, res) => {
 
 
 
+
    app.get('/all-comments',async(req,res)=>{
     //  const {ideaId}=req.params
       const allComments= await commentCollection.find().toArray()
